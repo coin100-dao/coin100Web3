322417e4863c4f9745606073cb0f981820602a1d
mehyar500 <mehyar500@gmail.com>
2024-11-18 21:17:18 +0000

    shit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
@@ -16,6 +17,15 @@ module.exports = {
     amoy: {
       url: process.env.AMOY_RPC_URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 1442, // Ensure this is the correct Chain ID
     },
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 137, // Polygon Mainnet Chain ID
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_ETHERSCAN_API_KEY, // Add your Polygon Etherscan API Key
   },
 };
