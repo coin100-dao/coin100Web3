67e24744871501fd7fd30c5954b21b4a56bc9b4f
mehyar500 <mehyar500@gmail.com>
2024-11-23 13:11:58 +0000

    newnew

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,17 +19,33 @@ module.exports = {
   networks: {
     amoy: {
       url: process.env.AMOY_RPC_URL,
-      accounts: [`0x${process.env.DEVELOPER_WALLET_PRIVATE_KEY}`],
+      accounts: [
+        process.env.DEVELOPER_WALLET_PRIVATE_KEY.startsWith("0x")
+          ? process.env.DEVELOPER_WALLET_PRIVATE_KEY
+          : `0x${process.env.DEVELOPER_WALLET_PRIVATE_KEY}`,
+        process.env.LIQUIDITY_WALLET_PRIVATE_KEY.startsWith("0x")
+          ? process.env.LIQUIDITY_WALLET_PRIVATE_KEY
+          : `0x${process.env.LIQUIDITY_WALLET_PRIVATE_KEY}`,
+      ],
       chainId: 80002,
     },
     polygon: {
       url: process.env.POLYGON_RPC_URL,
-      accounts: [`0x${process.env.DEVELOPER_WALLET_PRIVATE_KEY}`],
+      accounts: [
+        process.env.DEVELOPER_WALLET_PRIVATE_KEY.startsWith("0x")
+          ? process.env.DEVELOPER_WALLET_PRIVATE_KEY
+          : `0x${process.env.DEVELOPER_WALLET_PRIVATE_KEY}`,
+        process.env.LIQUIDITY_WALLET_PRIVATE_KEY.startsWith("0x")
+          ? process.env.LIQUIDITY_WALLET_PRIVATE_KEY
+          : `0x${process.env.LIQUIDITY_WALLET_PRIVATE_KEY}`,
+      ],
       chainId: 137, // Polygon Mainnet Chain ID
     },
     localhost: {
       url: "http://127.0.0.1:8545/",
-      accounts: { mnemonic: "test test test test test test test test test test test junk" },
+      accounts: [
+        "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", // Sample Private Key
+      ],
     },
   },
 };
