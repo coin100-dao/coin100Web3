6bf55648d164f5173ba4f40712a6f2b6ef27cad8
mehyar500 <mehyar500@gmail.com>
2024-11-27 21:09:24 +0000

    mm

--- contracts/COIN100 copy 12.sol
+++ contracts/COIN100 copy 12.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
+/**
+**COIN100** is a decentralized cryptocurrency index fund built on the polygon network. It represents the top 100 cryptocurrencies by market capitalization, offering users a diversified portfolio that mirrors the performance of the overall crypto market. Inspired by traditional index funds like the S\&P 500, COIN100
+
+**Ultimate Goal:** To dynamically track and reflect the top 100 cryptocurrencies by market capitalization, ensuring that COIN100 remains a relevant and accurate representation of the cryptocurrency market.
+*/
 pragma solidity ^0.8.20;
 
 // Import OpenZeppelin Contracts
